ebec3c84892a29a42a459cde4d84d589722a5159
Phat Lam <ltienphat1307@gmail.com>
2018-08-24 16:00:11 +0700

    fd

--- config/db.php
+++ config/db.php
@@ -1,24 +1,24 @@
 <?php
 
-if (YII_ENV_DEV) {
+// if (YII_ENV_DEV) {
+// 	return [
+// 	    'class' => 'yii\db\Connection',
+// 	    'dsn' => 'mysql:host=localhost;dbname=yii_basic',
+// 	    'username' => 'root',
+// 	    'password' => 'root',
+// 	    'charset' => 'utf8',
+
+// 	    // Schema cache options (for production environment)
+// 	    //'enableSchemaCache' => true,
+// 	    //'schemaCacheDuration' => 60,
+// 	    //'schemaCache' => 'cache',
+// 	];
+// }
+
+// if (YII_ENV_DEV) {
 	return [
 	    'class' => 'yii\db\Connection',
-	    'dsn' => 'mysql:host=localhost;dbname=yii_basic',
-	    'username' => 'root',
-	    'password' => 'root',
-	    'charset' => 'utf8',
-
-	    // Schema cache options (for production environment)
-	    //'enableSchemaCache' => true,
-	    //'schemaCacheDuration' => 60,
-	    //'schemaCache' => 'cache',
-	];
-}
-
-if (YII_ENV_PROD) {
-	return [
-	    'class' => 'yii\db\Connection',
-	    'dsn' => 'mysql:host=databases.000webhost.com;dbname=id6896486_yii_basic',
+	    'dsn' => 'mysql:host=//databases.000webhost.com;dbname=id6896486_yii_basic;port=3306',
 	    'username' => 'id6896486_root',
 	    'password' => '12345678@Xx',
 	    'charset' => 'utf8',
@@ -28,5 +28,5 @@ if (YII_ENV_PROD) {
 	    'schemaCacheDuration' => 60,
 	    'schemaCache' => 'cache',
 	];
-}
+// }
 ?>
