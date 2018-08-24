8be3b5884d6a4a0f4c28988b1edebec9b8962c4c
Phat Lam <ltienphat1307@gmail.com>
2018-08-24 15:11:00 +0700

    add server db

--- config/db.php
+++ config/db.php
@@ -1,14 +1,32 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii_basic',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
+if (YII_ENV_DEV) {
+	return [
+	    'class' => 'yii\db\Connection',
+	    'dsn' => 'mysql:host=localhost;dbname=yii_basic',
+	    'username' => 'root',
+	    'password' => 'root',
+	    'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
+	    // Schema cache options (for production environment)
+	    //'enableSchemaCache' => true,
+	    //'schemaCacheDuration' => 60,
+	    //'schemaCache' => 'cache',
+	];
+}
+
+if (YII_ENV_PROD) {
+	return [
+	    'class' => 'yii\db\Connection',
+	    'dsn' => 'mysql:host=databases.000webhost.com;dbname=id6896486_yii_basic',
+	    'username' => 'id6896486_root',
+	    'password' => '12345678@Xx',
+	    'charset' => 'utf8',
+
+	    // Schema cache options (for production environment)
+	    'enableSchemaCache' => true,
+	    'schemaCacheDuration' => 60,
+	    'schemaCache' => 'cache',
+	];
+}
+?>
