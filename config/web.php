8d6fbad84f3ea691cc6a254e5d25bf22ea277b1c
Phat Lam <ltienphat1307@gmail.com>
2018-08-25 16:04:52 +0700

    add debugger

--- config/web.php
+++ config/web.php
@@ -65,6 +65,7 @@ $config = [
     'params' => $params,
 ];
 
+if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
@@ -72,8 +73,7 @@ $config = [
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
-    
-if (YII_ENV_DEV) {
+
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
